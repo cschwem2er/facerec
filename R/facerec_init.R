7b1323767dbadac7f69ee5133af5cf533487d5c0
Carsten Schwemmer <carsten.schwemmer@methodds.de>
2018-05-11 23:11:28 +0200

    initial commit

--- R/facerec_init.R
+++ R/facerec_init.R
@@ -1,17 +1,10 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
 
 
 facerec_init <- function() {
